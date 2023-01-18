a1a3c0d2dfef7a171240ce1354bc1f8f9aa1de5a
GET Academy <terje@getacademy.no>
2023-01-18 09:24:02 +0100

    Initial commit

---
+++ BouncingBall/Ball.cs
@@ -0,0 +1,33 @@
+﻿namespace BouncingBall
+{
+    internal class Ball
+    {
+        private int _col;
+        private int _row;
+        private int _speedCol;
+        private int _speedRow;
+
+        public Ball(int col, int row, int speedCol, int speedRow)
+        {
+            _col = col;
+            _row = row;
+            _speedCol = speedCol;
+            _speedRow = speedRow;
+        }
+
+        public void Show()
+        {
+            Console.CursorLeft = _col;
+            Console.CursorTop = _row;
+            Console.Write("O");
+        }
+
+        public void Move()
+        {
+            _col += _speedCol;
+            _row += _speedRow;
+            if (_col <= 0 || _col >= Console.WindowWidth-1) _speedCol = -_speedCol;
+            if (_row <= 0 || _row >= Console.WindowHeight-1) _speedRow = -_speedRow;
+        }
+    }
+}
---
+++ BouncingBall/Program.cs
@@ -0,0 +1,11 @@
+﻿using BouncingBall;
+
+var ball = new Ball(15, 3, 1, 1);
+Console.CursorVisible = false;
+while (true)
+{
+    Console.Clear();
+    ball.Show();
+    ball.Move();
+    Thread.Sleep(50);
+}
