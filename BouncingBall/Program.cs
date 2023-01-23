1790bbdca3e721a4339844ae354e566ed87a7d5e
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2023-01-23 16:01:28 +0100

    flere rektangler med random parametre

--- BouncingBall/Program.cs
+++ BouncingBall/Program.cs
@@ -1,11 +1,23 @@
 ﻿using BouncingBall;
 
-var ball = new Ball(15, 3, 1, 1);
+var random = new Random();
+var rectangleList = new List<BouncingBall.Rectangle>();
+
+for (int i = 0; i < random.Next(3, 7); i++)
+{
+    var rectangle = new Rectangle(random.Next(1, 100), random.Next(1, 15), 1, 1, random.Next(3, 10), random.Next(3, 10));
+    rectangleList.Add(rectangle);
+}
+
 Console.CursorVisible = false;
 while (true)
 {
     Console.Clear();
-    ball.Show();
-    ball.Move();
+    foreach (var rectangle in rectangleList)
+    {
+        rectangle.Show();
+        rectangle.Move();
+    }
+
     Thread.Sleep(50);
 }
---
+++ BouncingBall/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿namespace BouncingBall
+{
+    internal class Rectangle
+    {
+        private int _col;
+        private int _row;
+        private int _speedCol;
+        private int _speedRow;
+        private int _width;
+        private int _height;
+
+        public Rectangle(int col, int row, int speedCol, int speedRow, int width, int height)
+        {
+            _col = col;
+            _row = row;
+            _speedCol = speedCol;
+            _speedRow = speedRow;
+            _width = width;
+            _height = height;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.CursorLeft = _col + i;
+                    Console.CursorTop = _row + j;
+                    Console.Write("M");
+                }
+            }
+        }
+
+        public void Move()
+        {
+            _col += _speedCol;
+            _row += _speedRow;
+            if (_col <= 0 || _col >= Console.WindowWidth - 4) _speedCol = -_speedCol;
+            if (_row <= 0 || _row >= Console.WindowHeight - 3) _speedRow = -_speedRow;
+        }
+    }
+}
