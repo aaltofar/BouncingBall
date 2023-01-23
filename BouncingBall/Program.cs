ba12b674c2944f41f31a93ecd898b5a99eea0b45
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2023-01-23 15:16:52 +0100

    nested for loops for bredde og høyde, reflektert i metoden Move for riktig collision check

--- BouncingBall/Program.cs
+++ BouncingBall/Program.cs
@@ -1,11 +1,26 @@
 ﻿using BouncingBall;
 
-var ball = new Ball(15, 3, 1, 1);
+var rectangle = new Rectangle(20, 10, 1, 1);
 Console.CursorVisible = false;
 while (true)
 {
     Console.Clear();
-    ball.Show();
-    ball.Move();
+    rectangle.Show();
+    rectangle.Move();
     Thread.Sleep(50);
 }
+
+
+
+//Oppgave 1
+//Ta utgangspunkt i denne koden: github.com / GetAcademy / BouncingBall
+//Skriv om så det er et rektangel med bredde 4 og høyde 3 som spretter rundt. 
+//    Du kan tegne et rektangel med en bokstav du velger, for eksempel med #:
+//####
+//####
+//####
+//Endre Ball til en klasse Rectangle
+//Sprett i høyre kant skjer når høyre kant av rektangelet når høyre kant
+//    Sprett i venstre kant skjer når venstre kant av rektangelet når venstre kant 
+//Sprett i øvre kant skjer når øvre kant av rektangelet når øvre kant 
+//    Sprett i nedre kant skjer når nedre kant av rektangelet når nedre kant 
---
+++ BouncingBall/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿namespace BouncingBall
+{
+    internal class Rectangle
+    {
+        private int _col;
+        private int _row;
+        private int _speedCol;
+        private int _speedRow;
+
+        public Rectangle(int col, int row, int speedCol, int speedRow)
+        {
+            _col = col;
+            _row = row;
+            _speedCol = speedCol;
+            _speedRow = speedRow;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.CursorLeft = _col + i;
+                    Console.CursorTop = _row + j;
+                    Console.Write("M");
+                }
+            }
+        }
+
+        public void Move()
+        {
+            _col += _speedCol;
+            _row += _speedRow;
+            if (_col <= 0 || _col >= Console.WindowWidth - 4) _speedCol = -_speedCol;
+            if (_row <= 0 || _row >= Console.WindowHeight - 3) _speedRow = -_speedRow;
+        }
+    }
+}
